1fe9fc63d24fa157d623aa70ad4b406684c8da4d
Daniel Flöter <d.floe@gmx.de>
2021-07-04 16:43:42 +0200

    Refactor block.json and update node packages

--- src/index.js
+++ src/index.js
@@ -7,22 +7,16 @@ import './style.scss';
 import Edit from './edit';
 import save from './save';
 
+import metadata from './../block.json';
+const { name } = metadata;
+
 /**
  * Every block starts by registering a new block type definition.
  *
  * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
  */
-registerBlockType( 'tiptip/category-archives-block', {
-	/**
-	 * @see https://make.wordpress.org/core/2020/11/18/block-api-version-2/
-	 */
-	apiVersion: 2,
-
-	/**
-	 * This is the display title for your block, which can be translated with `i18n` functions.
-	 * The block inserter will show this name.
-	 */
-	title: __( 'Category Archives', 'category-archives-block' ),
+registerBlockType( name, {
+	...metadata,
 
 	/**
 	 * This is a short description for your block, can be translated with `i18n` functions.
@@ -49,26 +43,6 @@ registerBlockType( 'tiptip/category-archives-block', {
 			showPostCounts: 
 				true,
 		},
-	},
-
-	/**
-	 * Blocks are grouped into categories to help users browse and discover them.
-	 * The categories provided by core are `text`, `media`, `design`, `widgets`, and `embed`.
-	 */
-	category: 'widgets',
-
-	/**
-	 * An icon property should be specified to make it easier to identify a block.
-	 * These can be any of WordPress’ Dashicons, or a custom svg element.
-	 */
-	icon: 'archive',
-
-	/**
-	 * Optional block extended support features.
-	 */
-	supports: {
-		// Removes support for an HTML mode.
-		html: false,
 	},
 
 	/**
